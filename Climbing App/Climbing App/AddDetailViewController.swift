e1a5878bee06ea0c7b294e1f34466ca04af44f80
Min Hu <huminh@Mins-MacBook-Air.local>
2015-06-17 21:11:26 -0700

    Got level scrollview to work

--- Climbing App/Climbing App/AddDetailViewController.swift
+++ Climbing App/Climbing App/AddDetailViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class AddDetailViewController: UIViewController {
+
 	
 	var routeName: String!
+	var selectedLevel : Int!
 	var level: String!
 	@IBOutlet weak var routeNameLabel: UILabel!
 	@IBOutlet weak var levelLabel: UILabel!
+	@IBOutlet weak var levelScrollView: UIScrollView!
+	@IBOutlet weak var touchableView: UIView!
+	var levels = ["5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.14d"]
+	
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		println(levelLabel)
 		routeNameLabel.text = routeName
 		levelLabel.text = level
-
+		levelScrollView.contentSize = CGSizeMake(2160, 80)
+		levelScrollView.clipsToBounds = false
+		touchableView.addGestureRecognizer(levelScrollView.panGestureRecognizer)
+		findSelectedlevel()
+		println(selectedLevel)
+		levelScrollView.contentOffset.x = CGFloat(116 * (selectedLevel-1))
         // Do any additional setup after loading the view.
     }
 
@@ -30,6 +42,14 @@ class AddDetailViewController: UIViewController {
 	
 	@IBAction func didPressCancelButton(sender: AnyObject) {
 		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+	func findSelectedlevel(){
+		for (var i = 0; i < levels.count; i++){
+			if levels[i] == levelLabel.text {
+				selectedLevel = i
+			}
+		}
 	}
 	
 
