9acc6b771ac76d1bf2510408c37a5ff293cf6a6f
Min Hu <huminh@Mins-MacBook-Air.local>
2015-06-18 22:20:25 -0700

    made some minor changes

--- Climbing App/Climbing App/AddDetailViewController.swift
+++ Climbing App/Climbing App/AddDetailViewController.swift
@@ -110,6 +110,7 @@ class AddDetailViewController: UIViewController, UIScrollViewDelegate {
 			label.transform = CGAffineTransformScale(label.transform, 1.2, 1.2)
 			
 			levelLabels[previousPageNumber].transform = CGAffineTransformIdentity
+			levelLabels[previousPageNumber]
 			
 			previousPageNumber = currentPageNumber
 		}
