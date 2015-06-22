1518a70efb254945e19630ad07096ac081fb9038
ceceyu <ceceyu@gmail.com>
2015-06-22 03:04:33 -0700

    adds loading screen and swipable tour but no page control yet

---
+++ Climbing-App/Climbing App/TourViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TourViewController.swift
+//  Climbing App
+//
+//  Created by Cece Yu on 6/22/15.
+//  Copyright (c) 2015 Min Hu. All rights reserved.
+//
+
+import UIKit
+
+class TourViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: 1600, height: 386)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
